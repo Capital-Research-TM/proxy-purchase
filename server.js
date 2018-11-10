101a36f25d7c7a1d3d630c23618906eb7daccafe
Alan <al.zhang93@gmail.com>
2018-11-10 10:20:39 -0800

    added dockerfile

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const path = require('path');
 const rp = require('request-promise');
 
-// use express.static to load index.html
-
-
 const app = express();
 const port = 3000;
 app.listen(port, () => {
